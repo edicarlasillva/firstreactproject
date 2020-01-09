1bc34a7368b00b387c64af8ed45e40b4a6c6358e
Edicarla Sillva <edicarlasillva@gmail.com>
2020-01-09 17:41:31 -0300

    List repositories

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+import { Container, Form, SubmitButton, List } from './styles';
 import api from '../../services/api';
 
 export default class Main extends Component {
@@ -32,7 +32,7 @@ export default class Main extends Component {
     console.log(this.state.repositories);
   };
   render() {
-    const { newRepo, loading } = this.state;
+    const { newRepo, loading, repositories } = this.state;
     return (
       <Container>
         <h1>
@@ -54,6 +54,15 @@ export default class Main extends Component {
             )}
           </SubmitButton>
         </Form>
+
+        <List>
+          {repositories.map(repository => (
+            <li key={repository.name}>
+              <span>{repository.name}</span>
+              <a href="#">Detalhes</a>
+            </li>
+          ))}
+        </List>
       </Container>
     );
   }
